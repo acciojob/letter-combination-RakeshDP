5f6d15fb2a4854a5a9f97600eccf46329eaefe5d
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-06-22 23:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function letterCombinations(input_digit) {
   //Complete the function
+   if (digits === "") {
+    return [];
+  }
+  const mappings = {
+    0: "0",
+    1: "1",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+  const result = [];
+  // Recursive function to generate combinations
+  function generateCombinations(current, remaining) {
+    if (remaining.length === 0) {
+      result.push(current);
+      return;
+    }
+    const currentDigit = parseInt(remaining[0]);
+    const letters = mappings[currentDigit];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      generateCombinations(current + letter, remaining.slice(1));
+    }
+  }
+
+  generateCombinations("", input_digit);
+
+  return result.sort();
 }
 
 module.exports = letterCombinations;
